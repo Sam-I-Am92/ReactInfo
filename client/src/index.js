3e5eefd2762aa20f38796974e045aa8eaef3c804
Sam-I-Am92 <pssandhu166@gmail.com>
2023-02-02 19:00:13 -0800

    Comment out store

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import Store from './store.js';
+// import Store from './store.js';
 import App from './components/App.js';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <Provider store={Store}>
+  // <Provider store={Store}>
       <App />
-  </Provider>
+  // </Provider>
 )
 
